ec3077f8cd44c8d89135ff0255b16fc1529d7ac4
Will <williamrwilson@gmail.com>
2015-04-21 00:14:39 +0000

    mild improvements to views

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,12 @@ class Account < ActiveRecord::Base
 
   has_many :checks
   
+  def total_expense
+    all_checks = checks.pluck(:amount)
+    all_checks.sum
+  end
+  
+  def current_balance
+    balance - self.total_expense
+  end
 end
--- app/models/check.rb
+++ app/models/check.rb
@@ -2,4 +2,6 @@ class Check < ActiveRecord::Base
   validates :date, :to, :amount, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0.01 }
   belongs_to :account
+  
+  
 end
