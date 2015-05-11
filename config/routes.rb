e066108613b0471fbf48ac5463e38b0e94be4e4d
Will <williamrwilson@gmail.com>
2015-05-11 01:04:52 +0000

    update route and production.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :accounts
   resources :deposits
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'accounts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
